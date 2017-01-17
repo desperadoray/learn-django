1921a54bf5c6c805bd9844beec3ec38d26ec3cfd
Ray <rza21.ca@gmail.com>
2017-01-17 01:54:58 -0800

    add models

--- summary/models.py
+++ summary/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	name = models.CharField(max_length = 200)
+	gender = models.CharField(max_length = 200)
+	relation = models.CharField(max_length = 200)
+
+class Event(models.Model):
+	person = models.ForeignKey(Person, on_delete = models.CASCADE)
+	event_name = models.CharField(max_length = 200)

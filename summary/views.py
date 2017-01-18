b8358ee79973f2dc75bd61373f0f0576b3db5a01
desperadoray <rza21.sfu@gmail.com>
2017-01-17 18:56:59 -0800

    add another view

--- summary/views.py
+++ summary/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Person
 from django.http import HttpResponse
 def index(request):
-	return HttpResponse("<h1>Summary</h1>")
+	all_Persons = Person.objects.all()
+	html = ''
+	for item in all_Persons:
+		url = '/summary/' + str(item.id) + '/'
+		html += '<a href = "' + url + '">' + item.name + '</a><br>'
+	return HttpResponse(html)
 
 def Person_detail(request, Person_id):
 	return HttpResponse("<h2>details for Person ID" + str(Person_id) +"</h2>")

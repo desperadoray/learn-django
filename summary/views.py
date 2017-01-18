9714e0158fd8678eeb7ed599b5c2a45aa6f9a4cd
desperadoray <rza21.ca@gmail.com>
2017-01-18 00:26:33 -0800

    add templates

--- summary/views.py
+++ summary/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 
 from .models import Person
 from django.http import HttpResponse
+from django.template import loader
+
 def index(request):
 	all_Persons = Person.objects.all()
-	html = ''
-	for item in all_Persons:
-		url = '/summary/' + str(item.id) + '/'
-		html += '<a href = "' + url + '">' + item.name + '</a><br>'
-	return HttpResponse(html)
+	#html = ''
+	#for item in all_Persons:
+	#	url = '/summary/' + str(item.id) + '/'
+	#	html += '<a href = "' + url + '">' + item.name + '</a><br>'
+	template = loader.get_template('summary/index.html')
+	context = {
+		'all_persons': all_Persons,
+	}
+	return HttpResponse(template.render(context,request))
 
 def Person_detail(request, Person_id):
 	return HttpResponse("<h2>details for Person ID" + str(Person_id) +"</h2>")

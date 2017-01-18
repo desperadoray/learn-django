447cff904a38458907130d755588666136e17471
desperadoray <rza21.ca@gmail.com>
2017-01-18 00:32:16 -0800

    shortcut render

--- summary/views.py
+++ summary/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from .models import Person
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+from django.shortcuts import render
 
 def index(request):
 	all_Persons = Person.objects.all()
@@ -10,11 +11,12 @@ def index(request):
 	#for item in all_Persons:
 	#	url = '/summary/' + str(item.id) + '/'
 	#	html += '<a href = "' + url + '">' + item.name + '</a><br>'
-	template = loader.get_template('summary/index.html')
+	#template = loader.get_template('summary/index.html')
 	context = {
 		'all_persons': all_Persons,
 	}
-	return HttpResponse(template.render(context,request))
+	#return HttpResponse(template.render(context,request))
+	return render(request, 'summary/index.html', context)
 
 def Person_detail(request, Person_id):
 	return HttpResponse("<h2>details for Person ID" + str(Person_id) +"</h2>")
